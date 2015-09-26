a52bb3222311a3c54a13ce79a006f6cedb5ba9d8
Công Phương Đông <gongfangdong@gmail.com>
2015-09-26 17:02:24 +0700

    Update
    Fix algorithm, add sound + animation

--- core/src/com/tatteam/popthecamera/actors/CameraButton.java
+++ core/src/com/tatteam/popthecamera/actors/CameraButton.java
@@ -13,10 +13,15 @@ import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 public class CameraButton extends Actor {
 
     private TextureRegion cameraButton;
+    private OnPressFinishListener listener;
 
     public CameraButton(TextureRegion cameraButton) {
         this.cameraButton = new TextureRegion(cameraButton);
         setBounds(getX(), getY(), cameraButton.getRegionWidth(), cameraButton.getRegionHeight());
+    }
+
+    public void setOnPressFinishListener(OnPressFinishListener listener) {
+        this.listener = listener;
     }
 
     public CameraButton(TextureRegion cameraButton, float width, float height, float x, float y) {
@@ -30,11 +35,22 @@ public class CameraButton extends Actor {
 
         MoveToAction action1 = new MoveToAction();
         action1.setPosition(defaultX, defaultY - 10f);
-        action1.setDuration(0.5f);
+        action1.setDuration(0.3f);
 
-        MoveToAction action2 = new MoveToAction();
+        MoveToAction action2 = new MoveToAction() {
+            @Override
+            public boolean act(float delta) {
+                boolean complete = super.act(delta);
+                if (complete) {
+                    if (listener != null) {
+                        listener.onPressFinish();
+                    }
+                }
+                return complete;
+            }
+        };
         action2.setPosition(defaultX, defaultY);
-        action2.setDuration(0.5f);
+        action2.setDuration(0.2f);
 
         addAction(new SequenceAction(action1, action2));
     }
@@ -45,5 +61,9 @@ public class CameraButton extends Actor {
         batch.setColor(color.r, color.g, color.b, color.a);
         batch.draw(cameraButton, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(),
                 getScaleX(), getScaleY(), getRotation());
+    }
+
+    public interface OnPressFinishListener {
+        void onPressFinish();
     }
 }
