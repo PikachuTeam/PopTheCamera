f303ad91be5631d8db5704fe14f30b6a985f4fb4
Thanh Nguyen Huy <thanhnh.411@gmail.com>
2015-09-29 20:42:41 +0700

    resolve conflict

--- desktop/src/com/tatteam/popthecamera/desktop/DesktopLauncher.java
+++ desktop/src/com/tatteam/popthecamera/desktop/DesktopLauncher.java
@@ -3,12 +3,13 @@ package com.tatteam.popthecamera.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tatteam.popthecamera.Constants;
+import com.tatteam.popthecamera.Main;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = Constants.DESKTOP_SCREEN_WIDTH;
         config.height = Constants.DESKTOP_SCREEN_HEIGHT;
-        new LwjglApplication(new PopTheCamera(), config);
+        new LwjglApplication(new Main(), config);
     }
 }
