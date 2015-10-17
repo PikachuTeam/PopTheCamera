cf5ee69a45cdb2e71256b9a59f1f74721dfbbd1b
Ngoc Thanh <lengocthanhit@gmail.com>
2015-10-18 00:10:45 +0700

    commit ios version

--- ios/src/com/tatteam/popthecamera/IOSLauncher.java
+++ ios/src/com/tatteam/popthecamera/IOSLauncher.java
@@ -1,20 +1,274 @@
 package com.tatteam.popthecamera;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.badlogic.gdx.utils.Logger;
+import org.robovm.apple.coregraphics.CGRect;
+import org.robovm.apple.coregraphics.CGSize;
 import org.robovm.apple.foundation.NSAutoreleasePool;
-import org.robovm.apple.uikit.UIApplication;
+import org.robovm.apple.foundation.NSErrorException;
+import org.robovm.apple.foundation.NSNumber;
+import org.robovm.apple.foundation.NSUserDefaults;
+import org.robovm.bindings.admob.*;
+import org.robovm.apple.uikit.*;
 
-public class IOSLauncher extends IOSApplication.Delegate {
+import java.util.Arrays;
+import java.util.List;
+
+public class IOSLauncher extends IOSApplication.Delegate  implements GDXGameLauncher.OnGameListener{
+    private static final Logger log = new Logger("AAA", Application.LOG_DEBUG);
+
+    private MainScreenViewController menuViewController;
+    private UIViewController rootViewController;
+    private IOSApplication app;
+    private GDXGameLauncher gameLauncher;
+
+    private GADBannerView adview;
+    private GADInterstitial interstitialAd;
+    private boolean adsInitialized = false;
+
+
+    private int lossGameCounter;
+    private final String small_banner_ad_unit_id = "ca-app-pub-4338754459250860/8940075638";
+    private final String big_banner_ad_unit_id ="ca-app-pub-4338754459250860/8783468432";
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new GDXGameLauncher(), config);
+        config.orientationLandscape=false;
+        config.orientationPortrait=true;
+        gameLauncher = new GDXGameLauncher();
+
+        gameLauncher.setGameMode(Constants.GameMode.CLASSIC_MEDIUM);
+
+        app =  new IOSApplication(gameLauncher, config);
+
+
+        return app;
     }
+
+
+
+
+    @Override
+    public void didBecomeActive (UIApplication application) {
+        if (null == menuViewController){
+            gameLauncher.setOnGameListener(this);
+            menuViewController = new MainScreenViewController();
+            menuViewController.setGameLauncher(gameLauncher);
+            menuViewController.setApp(this);
+            rootViewController = app.getUIWindow().getRootViewController();
+            rootViewController.presentViewController(menuViewController,false,null);
+
+
+        }
+
+        setupAds();
+    }
+
+    @Override
+    public void onLossGame(GDXGameLauncher gameLauncher, Constants.GameMode gameMode, int currentLevel, int score) {
+        displayAdsIfNeeded(gameMode, currentLevel, score);
+    }
+
+    @Override
+    public void onGameBackPressed(){
+        rootViewController.presentViewController(menuViewController,false,null);
+    };
+
+    private void displayAdsIfNeeded(Constants.GameMode gameMode, int currentLevel, int score) {
+
+        lossGameCounter++;
+        if (gameMode == Constants.GameMode.UNLIMITED) {
+            if (lossGameCounter % 5 == 0) {
+                displayAds();
+            }
+        } else {
+            if (gameMode == Constants.GameMode.CLASSIC_SLOW) {
+                if (currentLevel >= 5 && lossGameCounter % 4 == 0) {
+                    displayAds();
+                }
+            } else if (gameMode == Constants.GameMode.CLASSIC_MEDIUM) {
+                if (currentLevel >= 3 && lossGameCounter % 6 == 0) {
+                    displayAds();
+                }
+            } else if (gameMode == Constants.GameMode.CLASSIC_FAST) {
+                if (lossGameCounter % 6 == 0) {
+                    displayAds();
+                }
+            } else if (gameMode == Constants.GameMode.CLASSIC_CRAZY) {
+                if (lossGameCounter % 8 == 0) {
+                    displayAds();
+                }
+            }
+        }
+    }
+
+    private void displayAds() {
+
+
+
+        showFullAd();
+    }
+
+    private void showFullAd(){
+        log.debug("Show banner Ad");
+        if(interstitialAd.isReady()) {
+            interstitialAd.present(app.getUIViewController());
+        }
+
+    }
+    private void setupAds() {
+
+        interstitialAd = new GADInterstitial();
+        interstitialAd.setAdUnitID(big_banner_ad_unit_id);
+        requestNewInterstitial();
+        interstitialAd.setDelegate(new GADInterstitialDelegateAdapter(){
+            @Override
+            public void didFailToReceiveAd(GADInterstitial ad, GADRequestError error) {
+                log.debug(error.description());
+                requestNewInterstitial();
+            }
+        });
+    }
+
+    private void requestNewInterstitial() {
+        final GADRequest request = GADRequest.create();
+        List<String> testDevices = Arrays.asList(GADRequest.GAD_SIMULATOR_ID);
+        request.setTestDevices(testDevices);
+        interstitialAd.loadRequest(request);
+    }
+
+
+
 
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
     }
+
+
+
+
+
+
+
+
+
+    public void initializeAds() {
+        if (!adsInitialized) {
+
+
+            adsInitialized = true;
+
+            adview = new GADBannerView(GADAdSize.smartBannerPortrait());
+            if (adview != null){
+                adview.setAdUnitID(small_banner_ad_unit_id); //put your secret key here
+                adview.setRootViewController(rootViewController);
+                rootViewController.getView().addSubview(adview);
+
+                final GADRequest request = GADRequest.create();
+                List<String> testDevices = Arrays.asList(GADRequest.GAD_SIMULATOR_ID);
+                request.setTestDevices(testDevices);
+                adview.loadRequest(request);
+                adview.setDelegate(new GADBannerViewDelegateAdapter() {
+                    @Override
+                    public void didReceiveAd(GADBannerView view) {
+                        super.didReceiveAd(view);
+
+                    }
+
+                    @Override
+                    public void didFailToReceiveAd(GADBannerView view,
+                                                   GADRequestError error) {
+                        super.didFailToReceiveAd(view, error);
+                        log.debug(error.description());
+                        final GADRequest request = GADRequest.create();
+                        List<String> testDevices = Arrays.asList(GADRequest.GAD_SIMULATOR_ID);
+                        request.setTestDevices(testDevices);
+                        adview.loadRequest(request);
+
+                    }
+                });
+
+
+
+            }
+
+
+
+        }
+    }
+
+
+    public void showAds(boolean show) {
+        log.debug("Show banner Ad");
+        initializeAds();
+
+        final CGSize screenSize = UIScreen.getMainScreen().getBounds().getSize();
+        double screenWidth = screenSize.getWidth();
+        double screenHeight = screenSize.getHeight();
+
+        final CGSize adSize = adview.getBounds().getSize();
+        double adWidth = adSize.getWidth();
+        double adHeight = adSize.getHeight();
+
+
+
+        float bannerWidth = (float) screenWidth;
+        float bannerHeight = (float) (bannerWidth / adWidth * adHeight);
+
+        if(show) {
+            adview.setFrame(new CGRect((screenWidth / 2) - adWidth / 2, 0, bannerWidth, bannerHeight));
+        } else {
+            adview.setFrame(new CGRect(0, -bannerHeight, bannerWidth, bannerHeight));
+        }
+    }
+/*
+@Override
+    public void showInterAd() {
+        final GADInterstitial interstitial = new GADInterstitial(AD_UNIT_ID);
+        interstitial.setDelegate(new GADInterstitialDelegateAdapter(){
+            @Override
+            public void didReceiveAd(GADInterstitial ad) {
+                super.didReceiveAd(ad);
+                interstitial.present(((IOSApplication) Gdx.app).getUIViewController());
+            }
+
+            @Override
+            public void didFailToReceiveAd(GADInterstitial ad,
+                    GADRequestError error) {
+                Gdx.app.log("xxx", error.getCode() + "");
+                super.didFailToReceiveAd(ad, error);
+            }
+        });
+
+        GADRequest request = GADRequest.create();
+        List<String> devices = new ArrayList<String>();
+        devices.add(GADRequest.GAD_SIMULATOR_ID);
+
+        request.setTestDevices(devices);
+        interstitial.loadRequest(request);
+    }
+
+    @Override
+    public void showBanner() {
+         final GADBannerView mBannerView = new GADBannerView(GADAdSize.smartBannerPortrait());
+            mBannerView.setAdUnitID("ca-app-pub-1743332321423234/2574098708");
+
+            final UIViewController controller = ((IOSApplication) Gdx.app).getUIViewController();
+
+            GADRequest request = GADRequest.create();
+            List<String> devices = new ArrayList<String>();
+            devices.add(GADRequest.GAD_SIMULATOR_ID);
+            mBannerView.setRootViewController(controller);
+            controller.getView().addSubview(mBannerView);
+
+            mBannerView.loadRequest(request);
+    }
+}
+ */
+
 }
---
+++ ios/src/com/tatteam/popthecamera/MainScreenViewController.java
@@ -0,0 +1,167 @@
+package com.tatteam.popthecamera;
+
+import org.robovm.apple.foundation.NSData;
+import org.robovm.apple.foundation.NSURL;
+import org.robovm.apple.foundation.NSUserDefaults;
+import org.robovm.apple.uikit.*;
+import org.robovm.objc.annotation.CustomClass;
+
+import java.io.File;
+
+/**
+ * Created by admin on 10/7/15.
+ */
+
+@CustomClass("MainScreenViewController")
+public class MainScreenViewController extends UIViewController {
+
+
+    private GDXGameLauncher gameLauncher;
+    private IOSLauncher app;
+    public static final String KEY_GAME_MODE = "game_mode";
+    private static final String[] LEVEL_NAMES = {"Slow", "Medium", "Fast", "Crazy"};
+
+    private int levelIndex = 1;
+
+
+
+
+    private final UIImageView logo;
+    private final UIButton next;
+    private final UIButton prev;
+    private final UIButton start;
+    private final UIButton crazy;
+    private final UILabel mode;
+
+
+    public MainScreenViewController () {
+        super("ViewController", null);
+
+        UIView view = getView();
+        logo = (UIImageView) view.getViewWithTag(1);
+        next = (UIButton) view.getViewWithTag(2);
+        prev = (UIButton) view.getViewWithTag(3);
+        start = (UIButton) view.getViewWithTag(4);
+        crazy = (UIButton) view.getViewWithTag(5);
+        mode = (UILabel) view.getViewWithTag(6);
+
+
+//        [NSColor colorWithRed:0 green:0.51 blue:0.56 alpha:1]
+        view.setBackgroundColor(UIColor.fromRGBA(0,0.51,0.56,1));
+
+//        logo.setImage(img);
+
+
+
+
+        next.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+            @Override
+            public void onTouchUpInside (UIControl control, UIEvent event) {
+                playsound("change_level");
+                if (levelIndex >= LEVEL_NAMES.length - 1) {
+                    levelIndex = 0;
+                } else {
+                    levelIndex++;
+                }
+                updateLevelText();
+
+            }
+        });
+
+
+        prev.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+            @Override
+            public void onTouchUpInside (UIControl control, UIEvent event) {
+                playsound("change_level");
+                if (levelIndex <= 0) {
+                    levelIndex = LEVEL_NAMES.length - 1;
+                } else {
+                    levelIndex--;
+                }
+                updateLevelText();
+
+            }
+        });
+
+        start.setTitle("CLASSIC",UIControlState.Normal);
+
+        start.setTitleColor(UIColor.fromRGBA(0 , 0.31, 0.34, 1), UIControlState.Normal);
+        start.setTitleShadowColor(UIColor.fromWhiteAlpha(0.5, 1), UIControlState.Normal);
+
+        start.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+            @Override
+            public void onTouchUpInside (UIControl control, UIEvent event) {
+                if (gameLauncher != null){
+                    gameLauncher.setGameMode(getClassicGameMode());
+//                    gameLauncher.reset();
+                    dismissViewController(true,null);
+
+                }
+
+
+            }
+        });
+
+
+        crazy.setTitle("NO LIMIT",UIControlState.Normal);
+        crazy.setTitleColor(UIColor.fromRGBA(0 , 0.31, 0.34, 1), UIControlState.Normal);
+        crazy.setTitleShadowColor(UIColor.fromWhiteAlpha(0.5, 1), UIControlState.Normal);
+
+        crazy.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
+            @Override
+            public void onTouchUpInside (UIControl control, UIEvent event) {
+                if (gameLauncher != null){
+                    gameLauncher.setGameMode(Constants.GameMode.UNLIMITED);
+                    dismissViewController(true,null);
+                }
+
+
+            }
+        });
+
+        updateLevelText();
+    }
+
+    @Override
+    public void viewDidAppear(boolean animated) {
+        super.viewDidAppear(animated);
+//        NSUserDefaults defaults = NSUserDefaults.getStandardUserDefaults();
+//        int start = defaults.getInt("START_GAME");
+//        if (start > 5){
+//            app.showAds(true);
+//        }
+//        start += 1;
+//        defaults.put("START_GAME",start);
+//        defaults.synchronize();
+    }
+
+    private void updateLevelText() {
+        mode.setText(LEVEL_NAMES[levelIndex].toUpperCase());
+    }
+
+    private Constants.GameMode getClassicGameMode() {
+        switch (levelIndex) {
+            case 0:
+                return Constants.GameMode.CLASSIC_SLOW;
+            case 1:
+                return Constants.GameMode.CLASSIC_MEDIUM;
+            case 2:
+                return Constants.GameMode.CLASSIC_FAST;
+            case 3:
+                return Constants.GameMode.CLASSIC_CRAZY;
+        }
+        return Constants.GameMode.CLASSIC_MEDIUM;
+    }
+
+    private void playsound(String name){
+
+    }
+
+    public void setGameLauncher(GDXGameLauncher gameLauncher) {
+        this.gameLauncher = gameLauncher;
+    }
+
+    public void setApp(IOSLauncher app) {
+        this.app = app;
+    }
+}
